23c0f95cccc9f24881a1a569577ab899c270ab57
Avocado955 <mellis955@gmail.com>
2024-08-22 21:21:10 +1000

    adds project cards

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,13 +1,22 @@
 import styles from './ProjectCard.module.scss';
 
 interface ProjectCardProps {
+  project: {
+    name: string;
+    stack: string;
+    description: string;
+    link: string;
+  }
 }
 
-const ProjectCard = () => {
+const ProjectCard = ({project}: ProjectCardProps) => {
+  console.log(project);
   return (
-    <div>
-      <h1>{}</h1>
-
+    <div className={styles.card}>
+      <h1>{project.name}</h1>
+      <h3>{project.description}</h3>
+      <h4>{project.stack}</h4>
+      <a href={project.link} className={styles.link}>Link to Project</a>
     </div>
   )
 }
